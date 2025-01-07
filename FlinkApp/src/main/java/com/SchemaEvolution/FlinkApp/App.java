39448dea49af270abbea6139b6cefbf30e862e32
BarSnir <bar1991snir@gmail.com>
2025-01-07 19:34:02 +0000

    done with steps

--- FlinkApp/src/main/java/com/SchemaEvolution/FlinkApp/App.java
+++ FlinkApp/src/main/java/com/SchemaEvolution/FlinkApp/App.java
@@ -2,50 +2,47 @@ package com.SchemaEvolution.FlinkApp;
 
 import org.apache.flink.table.api.*;
 import org.apache.flink.table.expressions.TimeIntervalUnit;
-
 import static org.apache.flink.table.api.Expressions.*;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        EnvironmentSettings settings = EnvironmentSettings.inStreamingMode();
-        TableEnvironment tEnv = TableEnvironment.create(settings);
+        // EnvironmentSettings settings = EnvironmentSettings.inStreamingMode();
+        // TableEnvironment tEnv = TableEnvironment.create(settings);
+        
         // fetch from schema registry AVRO schema
         // detect schema change with curl
         // convert to schema string for kafka connector
         // execute sql source
-        tEnv.executeSql("CREATE TABLE transactions (\n" +
-        "    account_id  BIGINT,\n" +
-        "    amount      BIGINT,\n" +
-        "    transaction_time TIMESTAMP(3),\n" +
-        "    WATERMARK FOR transaction_time AS transaction_time - INTERVAL '5' SECOND\n" +
-        ") WITH (\n" +
-        "    'connector' = 'kafka',\n" +
-        "    'topic'     = 'transactions',\n" +
-        "    'properties.bootstrap.servers' = 'kafka:9092',\n" +
-        "    'format'    = 'csv'\n" +
-        ")");
+        // tEnv.executeSql("CREATE TABLE transactions (\n" +
+        // "    account_id  BIGINT,\n" +
+        // "    amount      BIGINT,\n" +
+        // "    transaction_time TIMESTAMP(3),\n" +
+        // "    WATERMARK FOR transaction_time AS transaction_time - INTERVAL '5' SECOND\n" +
+        // ") WITH (\n" +
+        // "    'connector' = 'kafka',\n" +
+        // "    'topic'     = 'transactions',\n" +
+        // "    'properties.bootstrap.servers' = 'kafka:9092',\n" +
+        // "    'format'    = 'csv'\n" +
+        // ")");
         // Update upcoming schema with lower-case
         // move desired compatibility mode 
         // execute sql ingest
-        tEnv.executeSql("CREATE TABLE transactions (\n" +
-        "    account_id  BIGINT,\n" +
-        "    amount      BIGINT,\n" +
-        "    transaction_time TIMESTAMP(3),\n" +
-        "    WATERMARK FOR transaction_time AS transaction_time - INTERVAL '5' SECOND\n" +
-        ") WITH (\n" +
-        "    'connector' = 'kafka',\n" +
-        "    'topic'     = 'transactions',\n" +
-        "    'properties.bootstrap.servers' = 'kafka:9092',\n" +
-        "    'format'    = 'csv'\n" +
-        ")");
+        // tEnv.executeSql("CREATE TABLE transactions (\n" +
+        // "    account_id  BIGINT,\n" +
+        // "    amount      BIGINT,\n" +
+        // "    transaction_time TIMESTAMP(3),\n" +
+        // "    WATERMARK FOR transaction_time AS transaction_time - INTERVAL '5' SECOND\n" +
+        // ") WITH (\n" +
+        // "    'connector' = 'kafka',\n" +
+        // "    'topic'     = 'transactions',\n" +
+        // "    'properties.bootstrap.servers' = 'kafka:9092',\n" +
+        // "    'format'    = 'csv'\n" +
+        // ")");
         // transform to lower-case & ingest
-        Table transactions = tEnv.from("transactions");
-        report(transactions).executeInsert("spend_report");
+        // Table transactions = tEnv.from("transactions");
+        // report(transactions).executeInsert("spend_report");
     }
 
     public static Table report(Table transactions) {
